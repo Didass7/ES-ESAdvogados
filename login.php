03747b4ca0c1341f2b47031258f76c19f7169fdf
Tiago Alves <tiagoalv3s28@gmail.com>
2025-03-24 13:11:11 +0000

    Pagina login

--- login.php
+++ login.php
@@ -4,17 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PaginaInicial</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 </head>
 
 <body>
 
-    <footer>
-    
+<div class="login-container">
+        <form>
+            <div class="form-group">
+                <div class= "input-icon">
+                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                <input type="text" id="username" name="username" placeholder="Insira o nome de utilizador" required>
+
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <i class="fa fa-lock input-icon"></i>
+                <input type="password" id="password" name="password" placeholder= "Insira a password" required>
+                            
+            </div>
+            
+            <button type="submit">Iniciar Sessão</button>
+        </form>
+    </div>
+
+
+
+
+
+<footer>
+      <a href="https://exemplo1.com" target="_blank">
+        <img src="placeholder.png" alt="Imagem 1">
+      </a>
+      <a href="https://exemplo2.com" target="_blank">
+        <img src="telephone.png" alt="Imagem 2">
+      </a>
+      <a href="https://exemplo3.com" target="_blank">
+        <img src="mail.png" alt="Imagem 3">
+      </a>    
     </footer>
 
 </body>
