c42a7cc577266e8a5dd071676753ac1a05581dc8
Rafa Cruz <rafael.lourenco.cruz20@gmail.com>
2025-05-30 19:46:25 +0100

    Refactor footer layout and styling across support, hours worked, and menu pages for improved consistency and user experience

--- horas_trabalhadas.php
+++ horas_trabalhadas.php
@@ -424,20 +424,25 @@ $result_horas = $stmt_horas->get_result();
         </div>
     </div>
 
-    <footer>
-        <div class="footer-images">
-            <a href="https://maps.app.goo.gl/UQYLoEsTwdgCKoft9" target="_blank">
-                <img src="location.png" alt="Imagem Localização">
-            </a>
-            <a href="https://moodle2425.ipcb.pt/" target="_blank">
-                <img src="phone.png" alt="Imagem Telefone">
-            </a>
-            <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox" target="_blank">
-                <img src="mail.png" alt="Imagem Mail">
-            </a>
-        </div>
-        <p class="copyright">© 2025 Todos os direitos reservados.</p>
-    </footer>
+<footer>
+  <div class="footer-content">
+    <div class="footer-text">HORAS TRABALHADAS</div>
+    <div class="footer-images">
+      <a href="https://maps.app.goo.gl/UQYLoEsTwdgCKoft9" target="_blank">
+          <img src="location.png" alt="Imagem Localização"/>
+        </a>
+        <a href="https://moodle2425.ipcb.pt/" target="_blank">
+          <img src="phone.png" alt="Imagem Telefone"/>
+        </a>
+        <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox" target="_blank">
+          <img src="mail.png" alt="Imagem Email"/>
+        </a>
+    </div>
+  </div>
+  <div class="copyright-wrapper">
+    <span class="copyright">© 2025 Todos os direitos reservados.</span>
+  </div>
+</footer>
 </body>
 </html>
 
